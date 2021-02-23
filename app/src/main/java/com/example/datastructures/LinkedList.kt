13f197297e5c97a02afc0283010a058a811bb720
Manisha <manisha.sharma@gmail.com>
2021-02-23 17:32:31 +0530

    used LinkedList class

--- app/src/main/java/com/example/datastructures/LinkedList.kt
+++ app/src/main/java/com/example/datastructures/LinkedList.kt
@@ -1,5 +1,7 @@
 package com.example.datastructures
 
+import java.util.*
+
 //LinkedList
 
 //Learning Linked List
@@ -24,6 +26,32 @@ class LinkedListExample {
             list.add(20)
             println(list.head!!.data)
             println(list.head!!.next!!.data)
+
+            //Java does all these works behind the sceen and provides us class called LinkedList
+            //ADD ITEMS
+
+            val list1 = LinkedList<String>()
+            list1.add("Java, C++")
+            list1.addFirst("C,Python")
+            list1.addLast("last element in the list")
+            println(list1)
+
+            //ACCESS ITEMS
+
+            println(list1.get(2))
+            println(list1.first)
+            println(list1.contains("Java, C++"))
+            println(list1)
+
+            //REMOVE
+            println("Removing items")
+            list1.remove()
+            println(list1)
+            list1.removeLast()
+            println(list1)
+
+            //print was showing that list is traversing from last
+
         }
     }
 }
